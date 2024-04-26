22bf3c6a763f7a3ea315a7b3f18aaadc757ff313
ansioses <ansioses@gmail.com>
2024-04-26 03:57:43 +0200

    cambio menu

--- resources/views/propiedades/formEdit.blade.php
+++ resources/views/propiedades/formEdit.blade.php
@@ -109,7 +109,7 @@
             </div>
 
             <div class="flex items-center justify-end mt-6 gap-x-6">
-                <a href="{{ route('propiedades.index') }}" class="btn btn-light" role="button">Cancelar</a>
+                <a href="{{ route('propiedades.index') }}" class="btn btn-light" role="button">Volver</a>
                 <button type="submit"
                     class="px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{ __('Guardar') }}</button>
             </div>
